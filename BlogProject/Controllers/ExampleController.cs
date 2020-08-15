67b7c6e9a13d0f190b9f64241260e37563af608d
Jacob Biggs <jbiggs@my.ancoraed.com>
2020-08-14 22:33:42 -0700

    Updated

---
+++ BlogProject/Controllers/ExampleController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogProject.Controllers
+{
+    public class ExampleController : Controller
+    {
+        public IActionResult Showmeapage()
+        {
+            return View();
+        }
+        public IActionResult ShowMeAnError()
+        {
+            return NotFound();
+        }
+
+        public IActionResult GiveMeAFile()
+        {
+            return File("/mydoc.txt", "text/plain");
+        }
+
+        public IActionResult GiveMeSomeJson()
+        {
+            string jsondata = "{Name:John-Luc Picard, Rank-Captain, Starship: Enterprise}";
+            return Json(jsondata);
+        }
+
+        public IActionResult TakeMeToGoogle()
+        {
+            return Redirect("https://Google.com");
+        }
+    }
+}
